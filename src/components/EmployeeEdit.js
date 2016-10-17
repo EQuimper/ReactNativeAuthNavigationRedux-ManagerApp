abb30816cc135d5fc0570e90207ac0d496a79715
EQuimper <quimperemanuel@gmail.com>
2016-10-17 07:08:28 -0400

    Now can sent text message + modal to confirm

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
+import { text } from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
+  state = { showModal: false };
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -15,15 +17,45 @@ class EmployeeEdit extends Component {
     const { name, phone, shift } = this.props;
     this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
   }
+  onTextPress() {
+    const { phone, shift } = this.props;
+
+    // Communications
+    text(phone, `Your upcoming shift is on ${shift}`);
+  }
   render() {
     return (
       <Card>
         <EmployeeForm />
+
         <CardSection>
           <Button onPress={() => this.onButtonPress()}>
             Save Changes
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.onTextPress()}>
+            Text Schedule
+          </Button>
+        </CardSection>
+
+        <CardSection>
+          <Button
+            onPress={() => this.setState({
+              showModal: !this.state.showModal
+            })}
+          >
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
+
       </Card>
     );
   }
